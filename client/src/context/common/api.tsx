9bd7c59affe740b6c93c8fcce217cb1f516bf884
Jabirrr <jabirmhd6786@gmail.com>
2024-05-12 16:17:06 +0530

    Done : password Gen Mini Project

---
+++ client/src/context/common/api.tsx
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+export const URL: string = "http://localhost:3002/api"
+
+const axiosInstance = axios.create({
+    baseURL: URL,
+    withCredentials: true
+})
+
+axiosInstance.interceptors.response.use((response) => {
+    return response.data;
+})
+
+export const commonRequest = async (method: string, url: string, data: object, headers: object) => {
+    const requestConfig = {
+        method,
+        url,
+        data,
+        headers
+    }
+
+    try {
+        const response = axiosInstance(requestConfig)
+        return response;
+    } catch (error) {
+        console.log(`hey safee...  something went wrong ${error}`);
+        return error;
+    }
+}
